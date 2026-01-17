95a3b29c60e047d0c5dd159e97383258cdc58f07
petr <petr@petr-rome2-epyc>
2026-01-17 12:11:37 +0100

    initial

---
+++ eu.kalafatic.evolution.view/src/eu/kalafatic/evolution/view/Perspective.java
@@ -0,0 +1,11 @@
+package eu.kalafatic.evolution.view;
+
+import org.eclipse.ui.IPageLayout;
+import org.eclipse.ui.IPerspectiveFactory;
+
+public class Perspective implements IPerspectiveFactory {
+
+	@Override	
+	public void createInitialLayout(IPageLayout layout) {
+	}
+}
